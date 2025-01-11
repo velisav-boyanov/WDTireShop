8e5aba4a2b0268e5f0abf2070be76f4255b0eb7e
velisav-boyanov <velislav.boyanov@gmail.com>
2025-01-12 00:31:10 +0200

    Page looks the same,  build is downloaded + project struct

---
+++ wd-spa/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+    <navbar
+    :pages="pages"
+    :active-page="activePage"
+    :nav-link-click="(index) => activePage = index">
+    </navbar>
+
+    <page-viewer
+    :page="pages[activePage]">
+    </page-viewer>
+</template>
+
+<script>
+import PageViewer from './components/PageViewer.vue';
+import Navbar from './components/Navbar.vue';
+
+    export default{
+        components:
+        {
+            Navbar,
+            PageViewer
+        },
+        data() {
+            return{
+                activePage : 0,
+                pages : [
+                    {
+                        link: {text: 'Home', url: 'index.html'},
+                        pageTitle: 'Home Page',
+                        content:  'This is the home page content'
+                    }, 
+                    {
+                        link: {text: 'About', url: 'about.html'},
+                        pageTitle: 'About Page',
+                        content:  'This is the about page content'
+                    },
+                    {
+                        link: {text: 'Contact', url: 'contact.html'},
+                        pageTitle: 'Contact Page',
+                        content:  'This is the contact page content'
+                    }
+                ]
+            };    
+        },
+    }
+</script>
